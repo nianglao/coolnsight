7f3c0541efcdc15350982f5182a0f543609e0ce3
lsun <lsun@freewheel.tv>
2019-03-22 15:40:22 +0800

    add home page

--- coolnsight/urls.py
+++ coolnsight/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
+    path('', views.home, name='main'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
