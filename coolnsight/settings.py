fe83d6d0efc9471b73fb9886881be3172ffa4240
lsun <lsun@freewheel.tv>
2019-03-22 18:09:59 +0800

    add ALLOWED_HOSTS

--- coolnsight/settings.py
+++ coolnsight/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c#q3p=9#o5a$l5t4!rzq&tjg)_1bhksmx*r&8$pnidyk#x8-$c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.coolnsight.com']
 
 
 # Application definition
